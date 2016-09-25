a0540fccadeb0d2291ccab6f1f5a8c42650fa510
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-09-25 14:50:41 -0400

    Initial Commit

---
+++ socket-print/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  socket-print
+//
+//  Created by Rudd Fawcett on 9/25/16.
+//  Copyright © 2016 Rudd Fawcett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
